4cb941675b65f97cce14122f3b7850076605a031
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-09 23:36:16 +0200

    page 5 release 1.0

--- src/main/java/com/pages/page5/Page5.java
+++ src/main/java/com/pages/page5/Page5.java
@@ -7,6 +7,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+import static org.opencv.imgcodecs.Imgcodecs.imwrite;
+import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
+import static org.opencv.imgproc.Imgproc.cvtColor;
+import com.constants.Constants;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FilenameUtils;
 
 public class Page5 extends JPanel implements Pages {
   private JFrame gui;
@@ -18,6 +30,9 @@ public class Page5 extends JPanel implements Pages {
   private JPanel buttonsPanel;
   private JButton openFileBtn;
   private JButton recognizeTextBtn;
+  private JFileChooser fileChooser;
+  private File file;
+  private String fileName;
 
 
   // Constructor
@@ -55,6 +70,8 @@ public class Page5 extends JPanel implements Pages {
     textArea.setBackground(Color.red);
     textArea.setPreferredSize(new Dimension(200,200));
     rightPanel.add(textArea);
+
+    fileChooser = new JFileChooser();
   }
 
 
@@ -63,6 +80,14 @@ public class Page5 extends JPanel implements Pages {
       @Override
       public void actionPerformed(ActionEvent e) {
         System.out.println("open");
+        if(fileChooser.showOpenDialog(gui) == JFileChooser.APPROVE_OPTION){
+          file = fileChooser.getSelectedFile();
+          // load the image
+          System.out.println("Image url: " + file);
+          fileName = file.getName();
+          System.out.println(file.getName());
+
+        }
       }
     });
 
@@ -70,8 +95,62 @@ public class Page5 extends JPanel implements Pages {
       @Override
       public void actionPerformed(ActionEvent e) {
         System.out.println("recognize");
+
+
+        System.out.println("Start recognize text from image");
+        long start = System.currentTimeMillis();
+
+        // Read image
+        Mat origin = imread(SRC_PATH + fileName);
+
+        String result = extractTextFromImage(origin);
+//        String result = new TextRecognizer().extractTextFromImage(origin);
+        System.out.println(result);
+
+        textArea.setText(result);
+
+        System.out.println("Time");
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("Done");
+
+        System.out.println("***************** "+FilenameUtils.removeExtension(fileName));
       }
     });
   }
+
+
+
+  ////////////////////////// RECOGNITION //////////////////////////
+  // Source path content images
+  static String SRC_PATH = Constants.projectPath+"\\img\\";
+  static String TESS_DATA = Constants.projectPath+"\\lib\\tesseract-OCR\\";    // path to Tesseract-OCR eng.trainedata
+
+  // Create tess obj
+  static Tesseract tesseract = new Tesseract();
+
+  // Load OPENCV
+  static {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    tesseract.setDatapath(TESS_DATA);
+  }
+
+  String extractTextFromImage(Mat inputMat) {
+    String result = "";
+    Mat gray = new Mat();
+
+    // Convert to gray scale
+    cvtColor(inputMat, gray, COLOR_BGR2GRAY);
+    imwrite(SRC_PATH + FilenameUtils.removeExtension(fileName) + "_gray.png", gray);
+
+    try {
+      // Recognize text with OCR
+      result = tesseract.doOCR(new File(SRC_PATH + FilenameUtils.removeExtension(fileName) +"_gray.png"));
+    } catch (TesseractException e) {
+      e.printStackTrace();
+    }
+
+    return result;
+  }
+
 
 }
