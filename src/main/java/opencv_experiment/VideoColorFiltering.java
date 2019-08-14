da198f9762f7ae3d11d86bd1e4a666c4eaa05ba4
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-14 13:21:38 +0200

    ImageContourFiltering

--- src/main/java/opencv_experiment/VideoColorFiltering.java
+++ src/main/java/opencv_experiment/VideoColorFiltering.java
@@ -1,34 +1,12 @@
 package opencv_experiment;
 
-//import com.googlecode.javacpp.Loader;
-//import com.googlecode.javacv.*;
-//import com.googlecode.javacv.cpp.*;
-//import com.googlecode.javacv.cpp.opencv_core.CvPoint;
-//import com.googlecode.javacv.cpp.opencv_core.CvScalar;
-//import com.googlecode.javacv.cpp.opencv_core.CvSeq;
-//import com.googlecode.javacv.cpp.opencv_core.IplImage;
-//import com.googlecode.javacv.cpp.opencv_highgui.CvCapture;
-//import com.googlecode.javacv.cpp.opencv_imgproc.CvMoments;
-//
-//import static com.googlecode.javacv.cpp.opencv_core.*;
-//import static com.googlecode.javacv.cpp.opencv_imgproc.*;
-//import static com.googlecode.javacv.cpp.opencv_calib3d.*;
-//import static com.googlecode.javacv.cpp.opencv_objdetect.*;
-//import static com.googlecode.javacv.cpp.opencv_highgui.*;
-
-import com.constants.Constants;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.IplImage;
-import org.bytedeco.javacv.FrameRecorder;
-import org.bytedeco.javacv.OpenCVFrameRecorder;
-import org.opencv.core.Core;
-
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2HSV;
 import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
 
-import org.opencv.videoio.VideoWriter;
 public class VideoColorFiltering {
 
 	/**
