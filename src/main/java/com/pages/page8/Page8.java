a972eb9cfba332dc635341a96271d9eb5daa5dde
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-20 21:32:29 +0200

    page8 webcam

--- src/main/java/com/pages/page8/Page8.java
+++ src/main/java/com/pages/page8/Page8.java
@@ -7,66 +7,69 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class Page8 extends JPanel implements Pages {
 
-  private Gui gui;
-  private JPanel tab;
-  private Webcam webcam = new Webcam();
-  private JButton startBtn = new JButton("Start");
-  private JButton pauseBtn = new JButton("Pause");
-  private JButton binaryFilter = new JButton("Binary Filter");
-  private JButton filterOffBtn = new JButton("FilterOff");
-  private boolean filtering = false;
-
-  public Page8(Gui gui) {
-    this.gui = gui;
-    this.tab = gui.getTab8();
-    initComponents();
-    addListeners();
-
-  }
-
-  private void addListeners() {
-
-    startBtn.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        webcam.on();
-      }
-    });
-
-    pauseBtn.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        webcam.off();
-      }
-    });
-
-    binaryFilter.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        webcam.binaryFilterOn();
-      }
-    });
-
-    filterOffBtn.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        webcam.filterOff();
-      }
-    });
-  }
+    private Gui gui;
+    private JPanel tab;
+    private Webcam webcam = new Webcam();
+    private JButton startBtn = new JButton("Start");
+    private JButton pauseBtn = new JButton("Pause");
+    private JButton binaryFilter = new JButton("Binary Filter");
+    private JButton filterOffBtn = new JButton("FilterOff");
+    private boolean filtering = false;
 
 
-  private void initComponents() {
-    JPanel mainPanel = new JPanel();
-    JPanel btnPanel = new JPanel();
-    tab.add(mainPanel);
-    tab.add(btnPanel);
-    mainPanel.add(webcam);
-    btnPanel.add(startBtn);
-    btnPanel.add(pauseBtn);
-    btnPanel.add(binaryFilter);
-    btnPanel.add(filterOffBtn);
-  }
+    public Page8(Gui gui) {
+        this.gui = gui;
+        this.tab = gui.getTab8();
+        initComponents();
+        addListeners();
+
+    }
+
+
+    private void addListeners() {
+
+        startBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                webcam.on();
+            }
+        });
+
+        pauseBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                webcam.off();
+            }
+        });
+
+        binaryFilter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                webcam.setFilter("gray");
+            }
+        });
+
+        filterOffBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                webcam.setFilter("hsv");
+            }
+        });
+    }
+
+
+    private void initComponents() {
+        JPanel mainPanel = new JPanel();
+        JPanel btnPanel = new JPanel();
+        tab.add(mainPanel);
+        tab.add(btnPanel);
+        mainPanel.add(webcam);
+        btnPanel.add(startBtn);
+        btnPanel.add(pauseBtn);
+        btnPanel.add(binaryFilter);
+        btnPanel.add(filterOffBtn);
+    }
 }
--- src/main/java/com/pages/page8/Webcam.java
+++ src/main/java/com/pages/page8/Webcam.java
@@ -1,8 +1,6 @@
 package com.pages.page8;
 
 import com.constants.Constants;
-
-import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 
 import javax.swing.*;
@@ -14,102 +12,101 @@ import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 public class Webcam extends JPanel implements Runnable {
-  protected volatile boolean runnable = false;
-  private int width = Constants.VIDEO_WIDTH;
-  private int height = Constants.VIDEO_HEIGHT;
-  private IplImage img, hsvImg, binImg, grayImg;
-  private CvCapture capture;
-  private String filter = "original";
 
-  public Webcam() {
-    setPreferredSize(new Dimension(width, height));
-  }
-
-  @Override
-  public void run() {
-    synchronized (this) {
-      hsvImg = cvCreateImage(cvSize(width, height), 8, 3);
-      binImg = cvCreateImage(cvSize(width, height), 8, 1);
-      capture = cvCreateCameraCapture(CV_CAP_ANY);
-      while (runnable) {
-        img = cvQueryFrame(capture);
-        if (img == null) {
-          break;
-        }
-
-        //hsv filter
-        cvCvtColor(img, hsvImg, CV_BGR2HSV);
-
-        //color filter
-        opencv_core.CvScalar minc = cvScalar(20, 100, 100, 0);
-        opencv_core.CvScalar maxc = cvScalar(60, 160, 160, 0);
-        cvInRangeS(hsvImg, minc, maxc, binImg);
-
-        //gray filter
-        grayImg = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);
-        cvCvtColor(img, grayImg, CV_BGR2GRAY);
-
-        //draw each image on JPanel
-        BufferedImage buff;
-        if (filter.equals("binary")) {
-          buff = grayImg.getBufferedImage();
-          //toDo add extra methods for each filter
-          //toDo add moments
-          // blue/green btn
-          // factory object recognition as on video, left item selected
-          // countur size experiment
-
-//          buff = hsvImg.getBufferedImage();
-//          buff = binImg.getBufferedImage();
-        } else {
-          buff = img.getBufferedImage();
-        }
+    protected volatile boolean runnable = false;
+    private int width = Constants.VIDEO_WIDTH;
+    private int height = Constants.VIDEO_HEIGHT;
+    private IplImage img, hsvImg, binImg, grayImg;
+    private CvCapture capture;
+    private CvScalar minc;
+    private CvScalar maxc;
+    private BufferedImage buffImg;
+    private String filter = "original";
 
 
-        this.getGraphics().drawImage(buff, 0, 0, width, height, 0, 0,
-                buff.getWidth(), buff.getHeight(), null);
-        if (!runnable) {
-//          cvReleaseImage(img);
-//          cvReleaseImage(hsvImg);
-//          cvReleaseImage(binImg);
-          cvReleaseCapture(capture);
-          break;
-        }
-      }
+    public Webcam() {
+        setPreferredSize(new Dimension(width, height));
+        minc = cvScalar(20, 100, 100, 0);
+        maxc = cvScalar(60, 160, 160, 0);
     }
-  }
 
-  public void binaryFilterOn(){
-    filter = "binary";
-  }
+    public void setFilter(String filterName) {
+        filter = filterName;
+    }
 
-  public void filterOff(){
-    filter = "original";
-  }
+    @Override
+    public void run() {
+        synchronized (this) {
+            hsvImg = cvCreateImage(cvSize(width, height), 8, 3);
+            binImg = cvCreateImage(cvSize(width, height), 8, 1);
+            capture = cvCreateCameraCapture(CV_CAP_ANY);
+            while (runnable) {
+                img = cvQueryFrame(capture);
+                if (img == null) {
+                    break;
+                }
+                //draw each image on JPanel
+                cvInRangeS(hsvImg, minc, maxc, binImg);
+                switch (filter) {
+                    case "original":
+                        buffImg = img.getBufferedImage();
+                        break;
+                    case "binary":
+                        buffImg = binImg.getBufferedImage();
+                        break;
+                    case "hsv":
+                        cvCvtColor(img, hsvImg, CV_BGR2HSV);
+                        buffImg = hsvImg.getBufferedImage();
+                        break;
+                    case "gray":
+                        grayImg = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);
+                        cvCvtColor(img, grayImg, CV_BGR2GRAY);
+                        buffImg = grayImg.getBufferedImage();
+                        break;
+                    default:
+                        buffImg = img.getBufferedImage();
+                        break;
+                }
 
-  public void on() {
-    Thread thread = new Thread(this);
-    thread.setDaemon(true);
-    this.runnable = true;
-    thread.start();
-  }
+                //toDo add extra methods for each filter
+                //toDo add moments
+                // blue/green btn
+                // factory object recognition as on video, left item selected
+                // countur size experiment
 
-  public void off() {
-    runnable = false;
-  }
-
-  // Getters
-  public IplImage getImg() {
-    return img;
-  }
-
-  public IplImage getHsvImg() {
-    return hsvImg;
-  }
-
-  public IplImage getBinImg() {
-    return binImg;
-  }
+                this.getGraphics().drawImage(buffImg, 0, 0, width, height, 0, 0,
+                        buffImg.getWidth(), buffImg.getHeight(), null);
+                if (!runnable) {
+                    //cvReleaseImage(img);
+                    //cvReleaseImage(hsvImg);
+                    //cvReleaseImage(binImg);
+                    cvReleaseCapture(capture);
+                    break;
+                }
+            }
+        }
+    }
 
 
+    public void on() {
+        Thread thread = new Thread(this);
+        thread.setDaemon(true);
+        this.runnable = true;
+        thread.start();
+    }
+
+    public void off() {
+        runnable = false;
+    }
+
+    // Getters
+    public IplImage getImg() {
+        return img;
+    }
+    public IplImage getHsvImg() {
+        return hsvImg;
+    }
+    public IplImage getBinImg() {
+        return binImg;
+    }
 }
