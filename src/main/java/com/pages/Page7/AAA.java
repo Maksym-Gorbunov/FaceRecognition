482afe3614d547f0f9047cfea34faf999ac45256
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-26 11:57:15 +0200

    experiment files was added

--- src/main/java/com/pages/Page7/AAA.java
+++ src/main/java/com/pages/Page7/AAA.java
@@ -1,59 +1,74 @@
-//package com.pages.Page7;
-//
-//import com.constants.Constants;
-//import org.bytedeco.javacpp.opencv_core;
-//import org.opencv.core.Core;
-//import org.opencv.core.CvType;
-//import org.opencv.core.Mat;
-//import org.opencv.imgcodecs.Imgcodecs;
-//import org.opencv.imgproc.Imgproc;
-//import org.bytedeco.javacpp.opencv_core.*;
-//import static org.bytedeco.javacpp.opencv_highgui.cvShowImage;
-//import static org.bytedeco.javacpp.opencv_imgproc.cvErode;
-//import static org.opencv.highgui.HighGui.imshow;
-//
-//import org.opencv.core.Core;
-//import org.opencv.core.CvType;
-//import org.opencv.core.Mat;
-//import org.opencv.imgcodecs.Imgcodecs;
-//i
-//
-//
-//
-//
-//public class AAA {
-//  public static void main(String[] args) {
-//    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//
-//    Mat img = Imgcodecs.imread(Constants.imgPath+"car3.png");
-//    Mat gray = new Mat();
-//    Mat draw = new Mat();
-//    Mat edge = new Mat();
-//
-//    Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
+package com.pages.Page7;
+
+import com.constants.Constants;
+import org.bytedeco.javacpp.opencv_core;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_highgui.cvShowImage;
+import static org.bytedeco.javacpp.opencv_imgproc.cvErode;
+import static org.opencv.highgui.HighGui.imshow;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+
+
+
+
+public class AAA {
+  public static void main(String[] args) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+    Mat img = Imgcodecs.imread(Constants.imgPath+"car3.png");
+    Mat gray = new Mat();
+    Mat draw = new Mat();
+    Mat edges = new Mat();
+
+    Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
+
+
+    int threshold = 100;
+    Imgproc.Canny(gray, edges, threshold, threshold*3);
+    edges.convertTo(draw, CvType.CV_8U);
+
+
+
+//    Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+
+
+
 //    Imgproc.Canny(gray, edge, 50, 150, 3, false);
-//    edge.convertTo(draw, CvType.CV_8U);
-//
-//
-//
-//
-//    if(Imgcodecs.imwrite(Constants.imgPath+"result\\gray.png", gray)){
-//      System.out.println("Gray saved to gray.png");
-//    }
-//
-//
+    if(Imgcodecs.imwrite(Constants.imgPath+"result\\gray.png", gray)){
+      System.out.println("..gray.png");
+    }
+    if(Imgcodecs.imwrite(Constants.imgPath+"result\\draw.png", draw)){
+      System.out.println("..draw.png");
+    }
+    if(Imgcodecs.imwrite(Constants.imgPath+"result\\edges.png", edges)){
+      System.out.println("..edges.png");
+    }
+
+
 //    IplImage i = draw;
-//
-//    //
-////    if(Imgcodecs.imwrite(Constants.imgPath+"result\\edge.png", draw)){
-////      System.out.println("Edge saved to edge.png");
-////    }
-//
-//
-//  }
-//
-//
-//
+
+    //
+//    if(Imgcodecs.imwrite(Constants.imgPath+"result\\edge.png", draw)){
+//      System.out.println("Edge saved to edge.png");
+//    }
+
+
+  }
+
+
+
 //  public BufferedImage Mat2BufferedImage(Mat m) {
 //    // Fastest code
 //    // output can be assigned either to a BufferedImage or to an Image
@@ -70,4 +85,4 @@
 //    System.arraycopy(b, 0, targetPixels, 0, b.length);
 //    return image;
 //  }
-//}
+}
