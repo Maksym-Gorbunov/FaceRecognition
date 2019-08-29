b75dbbf7ce899f5a6e912baf3d2df6aef4f89f30
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-29 15:31:27 +0200

    LPR ...

--- src/main/java/com/pages/Page7/FFF.java
+++ src/main/java/com/pages/Page7/FFF.java
@@ -7,8 +7,13 @@ import org.opencv.core.Core;
 public class FFF {
   public static void main(String[] args) {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    LicensePlateRecognition lpr = new LicensePlateRecognition();
-    lpr.findLicensePlate(Constants.imgPath+"cars\\1.jpg");
+//    LicensePlateRecognition lpr = new LicensePlateRecognition();
+//    lpr.findLicensePlate(Constants.imgPath+"cars\\6.jpg");
+    LPR lpr2 = new LPR();
+    lpr2.findLicensePlate(Constants.imgPath+"cars\\1.jpg");
+//    lpr2.findLicensePlate(Constants.imgPath+"cars\\test_004.jpg");
+
+//    System.out.println(lpr.recognizeText(Constants.imgPath+""));
   }
 }
 
