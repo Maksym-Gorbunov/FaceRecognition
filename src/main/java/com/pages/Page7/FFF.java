ce85c899d898c9d0be7b741b367009e30e8c742d
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-29 16:42:20 +0200

    LPR v3.0 thresh som arg

--- src/main/java/com/pages/Page7/FFF.java
+++ src/main/java/com/pages/Page7/FFF.java
@@ -10,9 +10,19 @@ public class FFF {
 //    LicensePlateRecognition lpr = new LicensePlateRecognition();
 //    lpr.findLicensePlate(Constants.imgPath+"cars\\6.jpg");
 
-    LPR lpr2 = new LPR();
-    lpr2.findLicensePlate(Constants.imgPath+"cars\\1.jpg", 100);
+    LicensePlateRecognizer lpr2 = new LicensePlateRecognizer();
+//    lpr2.findLicensePlate(Constants.imgPath+"cars\\1.jpg", 100);
     lpr2.findLicensePlate(Constants.imgPath+"cars\\6.jpg", 200);
+
+//    lpr2.findLicensePlate(Constants.imgPath+"cars\\test_001.jpg", 100);
+//    lpr2.findLicensePlate(Constants.imgPath+"cars\\test_001.jpg", 200);
+
+//    for (int i = 1; i <= 9; i++) {
+//      System.out.print(i+":  ");
+//      lpr2.findLicensePlate(Constants.imgPath + "cars\\test_00"+i+".jpg", 80);
+//
+//    }
+
   }
 }
 
