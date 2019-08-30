a1850f1d334d5548de6a8bdcd4cd13cd3ee708b1
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-08-30 13:30:00 +0200

    page7 ImgFile

--- src/main/java/com/pages/Page7/Page7.java
+++ src/main/java/com/pages/Page7/Page7.java
@@ -6,12 +6,11 @@ import com.gui.ImagePanel;
 import com.pages.Pages;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.awt.*;
 
 public class Page7 extends JPanel implements Pages {
@@ -20,22 +19,26 @@ public class Page7 extends JPanel implements Pages {
   private JPanel tab7;
   private JButton openBtn = new JButton("Open");
   private JButton recognizeBtn = new JButton("Recognize");
-//  private JFileChooser fileChooser = new JFileChooser();
-//  private List<File> files = new ArrayList<>();
-//  private File[] tempFiles;
-  private File file = null;
+//  private File file = null;
   private ImagePanel imagePanel;
   private JList jList;
-  private DefaultListModel<String> data = new DefaultListModel<>();
-  private JTextField resultField = new JTextField();
 
-  public Page7(Gui gui){
+  private DefaultListModel<ImgFile> data = new DefaultListModel<>();
+  private JTextField resultField = new JTextField();
+  private LicensePlateRecognizer recognizer;
+//  private String result;
+//  private String selectedImgFile = "";
+
+  private ImgFile selectedImgFile;
+
+
+  public Page7(Gui gui) {
     this.gui = gui;
     tab7 = gui.getTab7();
     initComponents();
     addListeners();
 
-
+    recognizer = new LicensePlateRecognizer();
 
 
   }
@@ -48,9 +51,10 @@ public class Page7 extends JPanel implements Pages {
         JFileChooser fc = new JFileChooser(Constants.imgPath);
         fc.setMultiSelectionEnabled(true);
         if (fc.showOpenDialog(gui) == JFileChooser.APPROVE_OPTION) {
-          File[] files = fc.getSelectedFiles();
-          for (File file : files){
-            data.addElement(file.getName());
+          File[] files =  fc.getSelectedFiles();
+          for (File file : files) {
+            ImgFile imgFile = new ImgFile(file.getAbsolutePath());
+            data.addElement(imgFile);
           }
         }
       }
@@ -60,8 +64,23 @@ public class Page7 extends JPanel implements Pages {
       @Override
       public void actionPerformed(ActionEvent e) {
 
+        System.out.println(data.get(jList.getSelectedIndex()));
+        System.out.println(data.get(jList.getSelectedIndex()).getLicenseNumber());
+
+//        result = recognizer.findLicensePlate(Constants.imgPath + "cars\\" + selectedImgFile, 100);
+//        resultField.setText(result);
       }
     });
+
+    jList.addListSelectionListener(new ListSelectionListener() {
+      @Override
+      public void valueChanged(ListSelectionEvent arg0) {
+        if (!arg0.getValueIsAdjusting()) {
+          selectedImgFile = data.get(jList.getSelectedIndex());
+        }
+      }
+    });
+
   }
 
   private void initComponents() {
@@ -76,7 +95,6 @@ public class Page7 extends JPanel implements Pages {
     JPanel mainRight = new JPanel();
 
 
-
     mainRight.setPreferredSize(new Dimension(Constants.FRAME_WIDTH - Constants.VIDEO_WIDTH, Constants.VIDEO_HEIGHT));
 
     mainRight.setBackground(Color.blue);
@@ -88,7 +106,7 @@ public class Page7 extends JPanel implements Pages {
     mainLeft.add(imagePanel);
     jList = new JList((ListModel) data);
     jList.setFixedCellWidth(20);
-    jList.setVisibleRowCount(5);
+    jList.setVisibleRowCount(20);
     jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     JScrollPane scrollPane = new JScrollPane(jList);
     jList.setFixedCellWidth(120);
@@ -100,12 +118,7 @@ public class Page7 extends JPanel implements Pages {
 
     mainRight.add(resultField);
     resultField.setText("res");
-    resultField.setPreferredSize(new Dimension(Constants.FRAME_WIDTH - Constants.VIDEO_WIDTH,20));
-
-
-
-
-
+    resultField.setPreferredSize(new Dimension(Constants.FRAME_WIDTH - Constants.VIDEO_WIDTH, 20));
 
 
     btnsPanel.add(openBtn);
