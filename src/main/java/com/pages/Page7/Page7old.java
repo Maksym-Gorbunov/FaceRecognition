95f932d9a9df5067f3c75ca1aebb7db51aa436fb
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-09-07 21:37:58 +0200

    contours

---
+++ src/main/java/com/pages/Page7/Page7old.java
@@ -0,0 +1,243 @@
+//package com.pages.Page7;
+//
+//import com.constants.Constants;
+//import com.gui.Gui;
+//import com.gui.ImagePanel;
+//import com.pages.Pages;
+//
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.io.File;
+//import javax.swing.*;
+//import javax.swing.border.EmptyBorder;
+//import javax.swing.event.ChangeEvent;
+//import javax.swing.event.ChangeListener;
+//import javax.swing.event.ListSelectionEvent;
+//import javax.swing.event.ListSelectionListener;
+//
+//public class Page7old extends JPanel implements Pages {
+//
+//  private JFrame gui;
+//  private JPanel tab7;
+//  private ImagePanel imgOriginalPanel;
+//  private ImagePanel imgFilteredPanel;
+//  private ImagePanel imgContoursPanel;
+//  private ImagePanel imgLicensePlatePanel;
+//  private JButton openBtn = new JButton("Open");
+//  private JButton recognizeBtn = new JButton("Recognize");
+//  private JButton rotationBtn = new JButton("rotation");
+//  private JList jList;
+//  private JTextField resultField = new JTextField("result field");
+//  private DefaultListModel<ImgObject> data = new DefaultListModel<>();
+//  private LicensePlateRecognizer recognizer;
+//  private String result;
+//  private ImgObject selectedImgFile;
+//  private JSlider thrashSlider;
+//  private JSlider blurSlider;
+//  private int width = 400;
+//  private int height = 300;
+//  boolean rotation = true;
+//
+//
+//
+//  public Page7(Gui gui) {
+//    this.gui = gui;
+//    tab7 = gui.getTab7();
+//    initComponents();
+//    addListeners();
+//
+//    recognizer = new LicensePlateRecognizer();
+//
+////    recognizer.ttt();
+//
+//    LPR lpr = new LPR();
+////    lpr.recognize(Constants.imgPath+"cars\\regnums\\COS799.jpg");
+//    lpr.recognize(Constants.imgPath+"cars\\regnums\\PUZ157.jpg");
+//  }
+//
+//  private void addListeners() {
+//    //open btn
+//    openBtn.addActionListener(new ActionListener() {
+//      @Override
+//      public void actionPerformed(ActionEvent e) {
+//        JFileChooser fc = new JFileChooser(Constants.imgPath + "cars\\");
+//        fc.setMultiSelectionEnabled(true);
+//        if (fc.showOpenDialog(gui) == JFileChooser.APPROVE_OPTION) {
+//          File[] files = fc.getSelectedFiles();
+//          for (File file : files) {
+//            ImgObject imgFile = new ImgObject(file.getAbsolutePath());
+//            data.addElement(imgFile);
+//          }
+//        }
+//        if (!data.isEmpty()) {
+//          jList.setSelectedIndex(0);
+//          recognizeBtn.setEnabled(true);
+//        }
+//      }
+//    });
+//
+//    //recognize btn
+//    recognizeBtn.addActionListener(new ActionListener() {
+//      @Override
+//      public void actionPerformed(ActionEvent e) {
+//        recognizeBtn.setEnabled(false);
+//        if (selectedImgFile != null) {
+//          int thresh = thrashSlider.getValue();
+//          int blur = blurSlider.getValue();
+//          result = recognizer.findLicensePlate(selectedImgFile.getAbsolutePath(), thresh, blur, rotation);
+//          resultField.setText(result);
+//          selectedImgFile.setLicenseNumber(result);
+//          if (recognizer.getFilteredImages()[0] != null && !recognizer.getFilteredImages()[0].empty()) {
+//            selectedImgFile.setThresholdImg(recognizer.getFilteredImages()[0]);
+//            imgFilteredPanel.loadMatImage(recognizer.getFilteredImages()[0]);
+//          } else {
+//            imgFilteredPanel.clear();
+//          }
+//          if (recognizer.getFilteredImages()[1] != null && !recognizer.getFilteredImages()[1].empty()) {
+//            selectedImgFile.setContoursImg(recognizer.getFilteredImages()[1]);
+//            imgContoursPanel.loadMatImage(recognizer.getFilteredImages()[1]);
+//          } else {
+//            imgContoursPanel.clear();
+//          }
+//          if (recognizer.getFilteredImages()[2] != null && !recognizer.getFilteredImages()[2].empty()) {
+//            selectedImgFile.setLicensePlateImg(recognizer.getFilteredImages()[2]);
+//            imgLicensePlatePanel.loadMatImage(recognizer.getFilteredImages()[2]);
+//          } else {
+//            imgLicensePlatePanel.clear();
+//          }
+//        }
+//      }
+//    });
+//
+//    //select list item
+//    jList.addListSelectionListener(new ListSelectionListener() {
+//      @Override
+//      public void valueChanged(ListSelectionEvent arg0) {
+//        if (!arg0.getValueIsAdjusting()) {
+//          recognizeBtn.setEnabled(true);
+//          selectedImgFile = data.get(jList.getSelectedIndex());
+//          imgOriginalPanel.loadImage(selectedImgFile);
+//          if (selectedImgFile.getThresholdImg() != null) {
+//            imgFilteredPanel.loadMatImage(selectedImgFile.getThresholdImg());
+//          } else {
+//            imgFilteredPanel.clear();
+//          }
+//          if (selectedImgFile.getContoursImg() != null) {
+//            imgContoursPanel.loadMatImage(selectedImgFile.getContoursImg());
+//          } else {
+//            imgContoursPanel.clear();
+//          }
+//          if (selectedImgFile.getLicensePlateImg() != null) {
+//            imgLicensePlatePanel.loadMatImage(selectedImgFile.getLicensePlateImg());
+//          } else {
+//            imgLicensePlatePanel.clear();
+//          }
+//
+//          resultField.setText(selectedImgFile.getLicenseNumber());
+//        }
+//      }
+//    });
+//
+//    thrashSlider.addChangeListener(new ChangeListener() {
+//      @Override
+//      public void stateChanged(ChangeEvent e) {
+//        recognizeBtn.setEnabled(true);
+//      }
+//    });
+//
+//    blurSlider.addChangeListener(new ChangeListener() {
+//      @Override
+//      public void stateChanged(ChangeEvent e) {
+//        recognizeBtn.setEnabled(true);
+//      }
+//    });
+//
+//  }
+//
+//
+//  private void initComponents() {
+//    JPanel top = new JPanel();
+//    JPanel bottom = new JPanel();
+//    JPanel topLeft = new JPanel();
+//    JPanel topRight = new JPanel();
+//    JPanel bottomLeft = new JPanel();
+//    JPanel bottomRight = new JPanel();
+//
+////    tab7.setLayout(new GridLayout(2,2));
+//
+//    top.add(topLeft);
+//    top.add(topRight);
+//    bottom.add(bottomLeft);
+//    bottom.add(bottomRight);
+//    tab7.add(top);
+//    tab7.add(bottom);
+//
+//    topLeft.setPreferredSize(new Dimension((int) (Constants.FRAME_WIDTH * 0.7), height + 40));
+//    topRight.setPreferredSize(new Dimension((int) (Constants.FRAME_WIDTH * 0.3), height + 40));
+//
+//    bottomLeft.setPreferredSize(new Dimension((int) (Constants.FRAME_WIDTH * 0.7), 200));
+//    bottomRight.setPreferredSize(new Dimension((int) (Constants.FRAME_WIDTH * 0.3), 200));
+//
+//    JTabbedPane imgTabPane = new JTabbedPane();
+//    imgOriginalPanel = new ImagePanel(width, height);
+//    imgFilteredPanel = new ImagePanel(width, height);
+//    imgContoursPanel = new ImagePanel(width, height);
+//    imgLicensePlatePanel = new ImagePanel(width, height);
+//    imgTabPane.add("Original", imgOriginalPanel);
+//    imgTabPane.add("Filtered", imgFilteredPanel);
+//    imgTabPane.add("Contours", imgContoursPanel);
+//    imgTabPane.add("LPlate", imgLicensePlatePanel);
+//
+////    topLeft.setBackground(Color.yellow);
+//
+//    topLeft.add(imgTabPane);
+//
+//
+//    topRight.setLayout(new BoxLayout(topRight, BoxLayout.Y_AXIS));
+//    topRight.setBorder(new EmptyBorder(10, 10, 10, 10));
+//    bottomLeft.setBorder(new EmptyBorder(0, 10, 0, 0));
+//
+//
+//    jList = new JList((ListModel) data);
+//    jList.setFixedCellWidth(20);
+//    jList.setVisibleRowCount(15);
+//    jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//    JScrollPane scrollPane = new JScrollPane(jList);
+//    jList.setFixedCellWidth(120);
+//    topRight.add(scrollPane);
+//    JLabel resultLabel = new JLabel("RESULT: ");
+//    topRight.add(resultLabel);
+//    topRight.add(resultField);
+//    JPanel btns = new JPanel();
+//    btns.add(openBtn);
+//    btns.add(recognizeBtn);
+//    topRight.add(btns);
+//
+////    bottomRight.add(openBtn);
+////    bottomRight.add(recognizeBtn);
+//
+//    bottomLeft.setLayout(new GridLayout(3, 2));
+//
+//    thrashSlider = new JSlider(JSlider.HORIZONTAL, 0, 250, 80);
+//    thrashSlider.setMinorTickSpacing(10);
+//    thrashSlider.setMajorTickSpacing(50);
+//    thrashSlider.setPaintTicks(true);
+//    thrashSlider.setPaintLabels(true);
+//    bottomLeft.add(thrashSlider);
+//    bottomLeft.add(new JLabel("Thresh"));
+//
+//    blurSlider = new JSlider(JSlider.HORIZONTAL, 0, 15, 3);
+//    blurSlider.setMinorTickSpacing(1);
+//    blurSlider.setMajorTickSpacing(5);
+//    blurSlider.setPaintTicks(true);
+//    blurSlider.setPaintLabels(true);
+//    bottomLeft.add(blurSlider);
+//    bottomLeft.add(new JLabel("Blur"));
+//
+//
+//    recognizeBtn.setEnabled(false);
+//  }
+//
+//
+//}
---
+++ src/main/java/com/pages/Page7/Recognizer.java
@@ -0,0 +1,185 @@
+package com.pages.Page7;
+
+import com.constants.Constants;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.opencv.core.*;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.imgproc.Imgproc.RETR_TREE;
+
+
+public class Recognizer {
+  private ImgObject object;
+  private Scalar blue = new Scalar(255, 0, 0, 255);
+  private Scalar green = new Scalar(0, 255, 0, 255);
+  private Scalar red = new Scalar(0, 0, 255, 255);
+  private Scalar randomColor = new Scalar(Math.random() * 255, Math.random() * 255, Math.random() * 255, 0);
+  private String outPath = Constants.imgPath + "lpr\\";
+
+
+  public static void main(String[] args) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    Recognizer ir = new Recognizer();
+    File f = new File(Constants.imgPath + "\\cars\\regnums\\YRR146.jpg");
+    int thresh = 100;
+    ir.recognize(f, thresh);
+  }
+
+
+  public void recognize(File file, int thresh) {
+    object = new ImgObject(file);
+    String fileNameWithOutExt = FilenameUtils.removeExtension(file.getName());
+    outPath = outPath + fileNameWithOutExt + "\\";
+    clearFolder(outPath);
+    Mat filtered = filterImage(object.getOriginal(), thresh);
+
+//    Mat filtered = new Mat();
+//    object.getOriginal().copyTo(filtered);
+//    filtered = filterImage(filtered, thresh);
+
+
+    object.setFiltered(filtered);
+    List<MatOfPoint> contours = new ArrayList<>();
+    contours = findContours(filtered);
+    if ((contours != null) && (contours.size() > 0)) {
+      System.out.println("total: " + contours.size());
+      int i = 0;
+      //work with valid contours
+      for (MatOfPoint c : contours) {
+        MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+        RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+        Point rotatedRectPoints[] = new Point[4];
+        rotatedRectangle.points(rotatedRectPoints);
+        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+
+        Mat contourMonoImg = new Mat(filtered, rect);
+        Mat contourColorImg = new Mat(object.getContours(), rect);
+        Imgcodecs.imwrite(outPath + "contourMono" + i + ".jpg", contourMonoImg);
+        Imgcodecs.imwrite(outPath + "contourColor" + i + ".jpg", contourColorImg);
+
+
+        i++;
+      }
+    }
+
+    object.saveImages(outPath);
+    System.out.println("...done...");
+  }
+
+
+  // Find contours on image
+  private List<MatOfPoint> findContours(Mat img) {
+    Mat filteredImg = object.getFiltered();
+    Mat contoursImg = new Mat();
+    object.getOriginal().copyTo(contoursImg);
+    List<MatOfPoint> contours = new ArrayList<>();
+    List<MatOfPoint> validContours = new ArrayList<>();
+    Imgproc.findContours(img, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+    for (MatOfPoint c : contours) {
+      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+      if ((rotatedRectangle.size.area() > 2000) && (rotatedRectangle.size.area() < 15000)) {
+        Point rotatedRectPoints[] = new Point[4];
+        rotatedRectangle.points(rotatedRectPoints);
+        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+        if ((rect.width > rect.height) && (rect.width < 6 * rect.height)) {
+          //toDo, check average color in contour, if white >50%
+          validContours.add(c);
+          Imgproc.rectangle(filteredImg, rect.tl(), rect.br(), red, 3);
+          Imgproc.rectangle(contoursImg, rect.tl(), rect.br(), red, 3);
+        }
+      }
+    }
+    if (validContours.size() > 0) {
+      object.setFiltered(filteredImg);
+      object.setContours(contoursImg);
+      return validContours;
+    }
+    return null;
+  }
+
+  // Rotate license plate image
+  private Mat rotateImage(Mat img, int angle) {
+    int rotatedAngle = 0;
+    if (angle == 0) {
+      return img;
+    }
+    if (angle < 0) {
+      rotatedAngle = 90 - Math.abs(angle);
+    }
+    if (angle > 0) {
+      rotatedAngle = -angle;
+    }
+    Mat temp = new Mat();
+    img.copyTo(temp);
+    Mat rotatedImg = new Mat(2, 3, CvType.CV_32FC1);
+    Mat destination = new Mat(img.rows(), img.cols(), img.type());
+    Point center = new Point(destination.cols() / 2, destination.rows() / 2);
+    rotatedImg = Imgproc.getRotationMatrix2D(center, rotatedAngle, 1);
+    Imgproc.warpAffine(temp, destination, rotatedImg, destination.size());
+    return destination;
+  }
+
+  // Filter main image, method translated from Python->C++
+  private Mat filterImage(Mat img, int thresh) {
+    int blurValue = 5;
+    Mat temp = new Mat();
+    img.copyTo(temp);
+    Mat gray = new Mat();
+    Mat topHat = new Mat();
+    Mat blackHat = new Mat();
+    Mat grayPlusTopHat = new Mat();
+    Mat grayPlusTopHatMinusBlackHat = new Mat();
+    Mat blur = new Mat();
+    Mat threshold = new Mat();
+    Mat kernel = new Mat(new Size(3, 3), CvType.CV_8U, new Scalar(255));
+    Imgproc.cvtColor(temp, gray, Imgproc.COLOR_RGB2GRAY);
+    Imgproc.morphologyEx(gray, topHat, Imgproc.MORPH_TOPHAT, kernel);
+    Imgproc.morphologyEx(gray, blackHat, Imgproc.MORPH_BLACKHAT, kernel);
+    Core.add(gray, topHat, grayPlusTopHat);
+    Core.subtract(grayPlusTopHat, blackHat, grayPlusTopHatMinusBlackHat);
+    Imgproc.GaussianBlur(grayPlusTopHatMinusBlackHat, blur, new Size(blurValue, blurValue), 1);
+    Imgproc.threshold(blur, threshold, thresh, 255, Imgproc.THRESH_BINARY_INV);
+    return threshold;
+  }
+
+  // Clear folder from old files
+  public void clearFolder(String path) {
+    try {
+      FileUtils.deleteDirectory(new File(path));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    new File(path).mkdirs();
+  }
+
+  // Convert Mat to BufferedImage
+  private BufferedImage Mat2BufferedImage(Mat matrix) throws Exception {
+    MatOfByte mob = new MatOfByte();
+    Imgcodecs.imencode(".jpg", matrix, mob);
+    byte ba[] = mob.toArray();
+    BufferedImage bi = ImageIO.read(new ByteArrayInputStream(ba));
+    return bi;
+  }
+
+  // Convert BufferedImage to Mat
+  private Mat bufferedImage2Mat(BufferedImage bi) {
+    Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC1);
+    //Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
+    byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+    mat.put(0, 0, data);
+    return mat;
+  }
+}
