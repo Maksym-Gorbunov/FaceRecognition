2bf8ccc13c63c7b5900ef95a1b648ea84104698e
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-09-14 23:54:26 +0200

    Page10 basic

---
+++ src/main/java/com/pages/page10/Recognizer.java
@@ -0,0 +1,157 @@
+package com.pages.page10;
+
+import com.constants.Constants;
+import org.apache.commons.io.FileUtils;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.imgproc.Imgproc.RETR_TREE;
+
+public class Recognizer {
+
+  private Mat frame = new Mat();
+  private Mat frameGray = new Mat();
+  private Mat bg;
+  private Mat bgGray = new Mat();
+  private Mat difference = new Mat();
+
+  private String path = Constants.imgPath + "HorseWebcam\\";
+  private int frameCounter = 0;
+
+
+  // Constructor
+  public Recognizer(Mat frame, int frameCounter, Mat bg) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    this.frame = frame;
+    clearFolder(path);
+    this.frameCounter = frameCounter;
+    this.bg = bg;
+  }
+
+  public static void main(String[] args) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+    String path = Constants.imgPath + "Horse\\";
+    String bgPath = path + "bg.jpg";
+    String framePath = path + "frame.jpg";
+
+    Mat bg = Imgcodecs.imread(bgPath);
+    Mat frame = Imgcodecs.imread(framePath);
+    Mat difference = new Mat();
+
+    Core.subtract(bg, frame, difference);
+    Mat differenceGray = new Mat();
+    Imgcodecs.imwrite(Constants.imgPath + "Horse\\result1.png", difference);
+    Imgproc.cvtColor(difference, differenceGray, Imgproc.COLOR_BGR2GRAY);
+    Imgcodecs.imwrite(path + "resultGray.jpg", differenceGray);
+
+    Mat differenceGrayThresh = new Mat();
+    //Imgproc.threshold(differenceGray, differenceGrayThresh, 10, 255, Imgproc.THRESH_BINARY_INV);
+
+    differenceGrayThresh = filterImage(difference, 10, 5);
+
+
+    Imgcodecs.imwrite(path + "resultGrayThresh.jpg", differenceGrayThresh);
+
+
+    List<MatOfPoint> contours = new ArrayList<>();
+    Imgproc.findContours(differenceGrayThresh, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+    RotatedRect maxRotatedRect = new RotatedRect();
+    maxRotatedRect = new RotatedRect();
+    double max = 0;
+
+    System.out.println("total: " + contours.size());
+    for (MatOfPoint c : contours) {
+      MatOfPoint2f points = new MatOfPoint2f(c.toArray());
+      RotatedRect rotatedRect = Imgproc.minAreaRect(points);
+      if ((rotatedRect.size.area() < differenceGrayThresh.size().area() * 0.8) && (max < rotatedRect.size.area())) {
+        max = rotatedRect.size.area();
+        maxRotatedRect = rotatedRect;
+      }
+    }
+//    System.out.println(maxRotatedRect.size.area());
+
+    System.out.println(maxRotatedRect.size.area());
+    Point maxRotatedRectPoints[] = new Point[4];
+    maxRotatedRect.points(maxRotatedRectPoints);
+    Rect rect = Imgproc.boundingRect(new MatOfPoint(maxRotatedRectPoints));
+    Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 255, 0, 255), 2);
+    Imgproc.rectangle(differenceGrayThresh, rect.tl(), rect.br(), new Scalar(0, 255, 0, 255), 2);
+
+
+    Imgcodecs.imwrite(path + "contours1.jpg", differenceGrayThresh);
+    Imgcodecs.imwrite(path + "contours.jpg", frame);
+  }
+
+  // Filter image
+  public static Mat filterImage(Mat img, int thresh, int blur) {
+    Mat tempImg = img.clone();
+    Mat grayImg = new Mat();
+    Mat topHatImg = new Mat();
+    Mat blackHatImg = new Mat();
+    Mat grayPlusTopHatImg = new Mat();
+    Mat grayPlusTopHatMinusBlackHatImg = new Mat();
+    Mat blurImg = new Mat();
+    Mat thresholdImg = new Mat();
+    Mat kernel = new Mat(new Size(3, 3), CvType.CV_8U, new Scalar(255));
+    Imgproc.cvtColor(tempImg, grayImg, Imgproc.COLOR_RGB2GRAY);
+    Imgproc.morphologyEx(grayImg, topHatImg, Imgproc.MORPH_TOPHAT, kernel);
+    Imgproc.morphologyEx(grayImg, blackHatImg, Imgproc.MORPH_BLACKHAT, kernel);
+    Core.add(grayImg, topHatImg, grayPlusTopHatImg);
+    Core.subtract(grayPlusTopHatImg, blackHatImg, grayPlusTopHatMinusBlackHatImg);
+    Imgproc.GaussianBlur(grayPlusTopHatMinusBlackHatImg, blurImg, new Size(blur, blur), 1);
+    Imgproc.threshold(blurImg, thresholdImg, thresh, 255, Imgproc.THRESH_BINARY_INV);
+    return thresholdImg;
+  }
+
+  public static Rect cutRectIfOutOfImageArea(Mat image, Rect rect) {
+    double startX = rect.tl().x;
+    double startY = rect.tl().y;
+    double endX = rect.br().x;
+    double endY = rect.br().y;
+    if (startX < 0) {
+      startX = 0;
+    }
+    if (startY < 0) {
+      startY = 0;
+    }
+    if (endX > image.width()) {
+      endX = image.width();
+    }
+    if (endY > image.height()) {
+      endY = image.height();
+    }
+    Rect cuttedRect = new Rect(new Point(startX, startY), new Point(endX, endY));
+    return cuttedRect;
+  }
+
+  public void test() {
+    if ((bg != null) && (!bg.empty())) {
+      System.out.println("bg");
+      Imgcodecs.imwrite(path+"bg.jpg", bg);
+    }
+    Imgcodecs.imwrite(path + "original.jpg", frame);
+  }
+
+  // Clear folder from old files
+  public void clearFolder(String path) {
+    try {
+      FileUtils.deleteDirectory(new File(path));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    new File(path).mkdirs();
+  }
+
+  // Save image
+  private void saveImage(String filename, Mat img) {
+    Imgcodecs.imwrite(path + filename + ".jpg", img);
+  }
+}
---
+++ src/main/java/com/pages/page10/WebcamPanel.java
@@ -0,0 +1,198 @@
+package com.pages.page10;
+
+import com.constants.Constants;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+
+import static org.bytedeco.javacpp.opencv_core.IplImage;
+
+
+public class WebcamPanel extends JPanel {
+  private static final long serialVersionUID = 1L;
+  private JLabel imageLabel;
+  private ImageIcon transformedImageIcon;
+  private int width;
+  private int height;
+  private BufferedImage bufferedImage;
+  private DaemonThread myThread = null;
+  private int count = 0;
+  private VideoCapture webSource = null;
+  private Mat frame = new Mat();
+  private MatOfByte mem = new MatOfByte();
+  private Graphics graphics;
+  private Mat bg = new Mat();
+
+  // Constructor
+  public WebcamPanel(int width, int height) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    this.imageLabel = new JLabel();
+    setLayout(new BorderLayout());
+    setBorder(BorderFactory.createLineBorder(Color.black));
+    add(imageLabel, BorderLayout.CENTER);
+    setBackground(Color.ORANGE);
+    setPreferredSize(new Dimension(width, height));
+    this.width = width;
+    this.height = height;
+  }
+
+//  public Mat getBg() {
+//    return bg;
+//  }
+
+  // Convert Mat to BufferedImage
+  static BufferedImage Mat2BufferedImage(Mat matrix) throws Exception {
+    if (matrix == null) {
+      return null;
+    }
+    MatOfByte mob = new MatOfByte();
+    Imgcodecs.imencode(".jpg", matrix, mob);
+    byte ba[] = mob.toArray();
+
+    BufferedImage bi = ImageIO.read(new ByteArrayInputStream(ba));
+    return bi;
+  }
+
+  public void setBg() {
+    System.out.println("***");
+    frame.copyTo(bg);
+  }
+  ///////////////////////////// class DaemonThread end //////////////////////////////////////
+
+  // Start showing video from webbcam
+  public void start() {
+    System.out.println("Start...");
+    webSource = new VideoCapture(0);
+    myThread = new DaemonThread();
+    Thread t = new Thread(myThread);
+    t.setDaemon(true);
+    myThread.runnable = true;
+    t.start();
+  }
+
+  // Pause video from webcam
+  public void stop() {
+    System.out.println("Pause...");
+    clear();
+    setBackground(Color.ORANGE);
+    myThread.runnable = false;
+    webSource.release();
+
+  }
+
+  // Update image
+  public void updadeImage(final Image image) {
+    imageLabel.setIcon(new ImageIcon(scaleImage(image)));
+  }
+
+  // Scale image
+  private Image scaleImage(Image image) {
+    return image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+  }
+
+  // Load image from file
+  public void loadImage(File file) {
+    this.transformedImageIcon = new ImageIcon(file.getAbsolutePath());
+    Image image = transformedImageIcon.getImage();
+    updadeImage(image);
+  }
+
+  // Clear panel
+  public void clear() {
+    imageLabel.setIcon(null);
+  }
+
+  // load image to ImagePanel without extra savers
+  public void loadImage(IplImage filteredImage) {
+    BufferedImage img1 = filteredImage.getBufferedImage();
+    ImageIcon icon = new ImageIcon(img1);
+    Image image = icon.getImage();
+    updadeImage(image);
+  }
+
+  // Load image from Mat
+  public void loadImage(Mat filteredImage) {
+    if (filteredImage == null) {
+      return;
+    }
+    BufferedImage img1 = null;
+    try {
+      img1 = Mat2BufferedImage(filteredImage);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    ImageIcon icon = new ImageIcon(img1);
+    Image image = icon.getImage();
+    updadeImage(image);
+  }
+
+  // Matrix image BlueGreenRed
+  public boolean convertMatToImage(Mat matBGR) {
+    int width = matBGR.width();
+    int height = matBGR.height();
+    int channels = matBGR.channels();
+    byte[] sourcePixels = new byte[width * height * channels];
+    matBGR.get(0, 0, sourcePixels);
+    bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+    final byte[] targetPixel = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+    System.arraycopy(sourcePixels, 0, targetPixel, 0, sourcePixels.length);
+    return true;
+  }
+
+  // Paint component
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    if (this.bufferedImage == null) {
+      return;
+    }
+    g.drawImage(this.bufferedImage, 10, 10, this.bufferedImage.getWidth(), this.bufferedImage.getHeight(), null);
+  }
+
+  ///////////////////////////// class DaemonThread start //////////////////////////////////////
+  class DaemonThread implements Runnable {
+
+    protected volatile boolean runnable = false;
+
+
+    @Override
+    public void run() {
+      synchronized (this) {
+        while (runnable) {
+          if (webSource.grab()) {
+            try {
+              webSource.retrieve(frame);
+
+              if(count == 200){
+                Recognizer recognizer = new Recognizer(frame, count, bg);
+                System.out.println(count);
+                recognizer.test();
+              }
+
+              Imgcodecs.imencode(".bmp", frame, mem);
+              BufferedImage buff = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
+              graphics = WebcamPanel.this.getGraphics();
+              if (graphics.drawImage(buff, 0, 0, Constants.VIDEO_WIDTH, Constants.VIDEO_HEIGHT, 0, 0, buff.getWidth(), buff.getHeight(), null))
+                if (runnable == false) {
+                  System.out.println("Going to wait()");
+                  this.wait();
+                }
+              count++;
+            } catch (Exception e) {
+              System.out.println("Error");
+            }
+          }
+        }
+      }
+    }
+  }
+}
