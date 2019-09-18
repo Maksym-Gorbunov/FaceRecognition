8cdf2c2f4a446577862a3f521c13b937fe02d65b
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-09-18 09:17:12 +0200

    TensorFlow

---
+++ src/main/java/com/pages/Page11/Page11.java
@@ -0,0 +1,82 @@
+package com.pages.page11;
+
+import com.constants.Constants;
+import com.gui.Gui;
+import com.pages.Page11.WebcamPanel;
+import com.pages.Pages;
+import org.opencv.core.Rect;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Page11 extends JPanel implements Pages {
+
+  public static Rect rect = null;
+  private Gui gui;
+  private JPanel tab11;
+  private WebcamPanel webcamPanel = new WebcamPanel(Constants.VIDEO_WIDTH, Constants.VIDEO_HEIGHT);
+  private JButton startBtn = new JButton("Start");
+  private JButton stopBtn = new JButton("Stop");
+  private JButton bgBtn = new JButton("Set background");
+
+  // Constructor
+  public Page11(Gui gui) {
+    this.gui = gui;
+    this.tab11 = gui.getTab11();
+    initComponents();
+    addListeners();
+  }
+
+
+  // Listeners
+  private void addListeners() {
+
+    //start
+    startBtn.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        startBtn.setEnabled(false);
+        webcamPanel.start();
+        stopBtn.setEnabled(true);
+        bgBtn.setEnabled(true);
+      }
+    });
+
+    //stop
+    stopBtn.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        stopBtn.setEnabled(false);
+        webcamPanel.stop();
+        startBtn.setEnabled(true);
+        bgBtn.setEnabled(false);
+      }
+    });
+
+    //set background, scene layout
+    bgBtn.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        webcamPanel.setBg();
+        bgBtn.setEnabled(false);
+      }
+    });
+
+  }
+
+
+  // Initialization
+  private void initComponents() {
+    JPanel mainPanel = new JPanel();
+    mainPanel.add(webcamPanel);
+    tab11.add(mainPanel);
+    JPanel btnsPanel = new JPanel();
+    btnsPanel.add(startBtn);
+    btnsPanel.add(stopBtn);
+    btnsPanel.add(bgBtn);
+    tab11.add(btnsPanel);
+    stopBtn.setEnabled(false);
+    bgBtn.setEnabled(false);
+  }
+}
