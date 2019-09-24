76ae35ea6557df62e2410694ba49a6002086dec2
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-09-24 14:41:58 +0200

    contours plate v1.0

--- src/main/java/com/pages/page9/Recognizer.java
+++ src/main/java/com/pages/page9/Recognizer.java
@@ -1,6 +1,7 @@
 package com.pages.page9;
 
 import com.constants.Constants;
+import com.pages.page8.Webcam;
 import org.apache.commons.io.FileUtils;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -43,23 +44,118 @@ public class Recognizer implements Runnable {
 
   public static void main(String[] args) {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    String pth = Constants.imgPath + "filterColor\\";
-    Mat frame = Imgcodecs.imread(pth + "1.jpg");
-//    Mat frame = Imgcodecs.imread(pth + "color.jpg");
+    String pth = Constants.imgPath + "aaa\\";
+    Mat frame = Imgcodecs.imread(pth + "33.jpg");
+//    Mat frame1 = Imgcodecs.imread(pth + "1.jpg");
+//    Mat frame2 = Imgcodecs.imread(pth + "2.jpg");
+//    Mat frame3 = Imgcodecs.imread(pth + "color.jpg");
     Recognizer r = new Recognizer(frame, 0, pth);
+    Mat gray = new Mat();
+    Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+    Mat filtered = r.filterImage(gray,100,5);
+    Imgcodecs.imwrite(pth+"filtered.jpg", filtered);
 
-    Scalar lowerBlue = new Scalar(100, 0, 0);
-    Scalar upperBlue = new Scalar(255, 255, 255);
+    r.getContoursFromPlate(frame,filtered);
 
-    Mat result = r.filterColor(frame, lowerBlue, upperBlue);
+
+
+//    //Scalar lowerBlue = new Scalar(15, 30, 110);
+//    //Scalar upperBlue = new Scalar(150, 255, 255);
+//    Scalar lowerBlue = new Scalar(20, 100, 100);
+//    Scalar upperBlue = new Scalar(255, 255, 255);
+//
+//
+//    Mat result1 = r.removeColor(frame1, lowerBlue, upperBlue);
+//    Imgcodecs.imwrite(pth+"result1.jpg", result1);
+//
+//    frame2.convertTo(frame2,-1,1,80);
+//    Mat result2 = r.removeColor(frame2, lowerBlue, upperBlue);
+//    Imgcodecs.imwrite(pth+"result2.jpg", result2);
+//
+//    Mat result3 = r.removeColor(frame3, lowerBlue, upperBlue);
+//    Imgcodecs.imwrite(pth+"result3.jpg", result3);
+//
+//
+////    Mat brigth = new Mat();
+////    frame2.convertTo(brigth,-1,1,100);
+////    Imgcodecs.imwrite(pth+"result1_bright.jpg", brigth);
+
+  }
+
+  private void getContoursFromPlate(Mat colorPlate, Mat filteredPlate){
+    List<MatOfPoint> tempContours = new ArrayList<>();
+    Mat colorPlateCopy = new Mat();
+    colorPlate.copyTo(colorPlateCopy);
+    Imgproc.findContours(filteredPlate, tempContours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+    int i = 1;
+    double startX = colorPlate.width();
+    double startY = colorPlate.height();
+    double endX = 0;
+    double endY = 0;
+    for (MatOfPoint c : tempContours) {
+      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+      Point rotatedRectPoints[] = new Point[4];
+      rotatedRectangle.points(rotatedRectPoints);
+      Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+
+      rect = cutRectIfOutOfImageArea(filteredPlate, rect);
+
+
+
+        if( (rect.height>0.5*filteredPlate.height()) && (rect.height<0.95*filteredPlate.height()) ) {
+
+          Imgproc.rectangle(colorPlate, rect.tl(), rect.br(), blue, 1);
+          System.out.println(rotatedRectangle.angle);
+
+          if(rect.tl().x<startX){
+            startX = rect.tl().x;
+          }
+          if(rect.tl().y<startY){
+            startY = rect.tl().y;
+          }
+
+          if(rect.br().x>endX){
+            endX = rect.br().x;
+          }
+          if(rect.br().y>endY){
+            endY = rect.br().y;
+          }
+
+
+        }
+
+    }
+    String pth = Constants.imgPath + "aaa\\";
+    Imgcodecs.imwrite(pth+"contours.jpg", colorPlate);
+
+    Point start = new Point(startX, startY);
+    Point end = new Point(endX, endY);
+
+    Imgproc.rectangle(colorPlate, start, end, red, 2);
+    Imgcodecs.imwrite(pth+"contours2.jpg", colorPlate);
+
+    Mat result = new Mat(colorPlateCopy, new Rect(start, end));
     Imgcodecs.imwrite(pth+"result.jpg", result);
 
 
   }
 
-  private Mat filterColor(Mat src, Scalar lowerColor, Scalar upperColor){
+  private Mat removeColor(Mat src, Scalar lowerColor, Scalar upperColor){
     Mat mask = new Mat();
     Core.inRange(src, lowerColor, upperColor, mask);
+    Imgproc.cvtColor(mask, mask, Imgproc.COLOR_GRAY2RGB);
+    Mat result = new Mat();
+    Core.subtract(mask, src, result);
+    Core.subtract(mask, result, result);
+    return result;
+  }
+
+  private Mat removeBlueColor(Mat src){
+    Scalar lowerBlue = new Scalar(15, 30, 110);
+    Scalar upperBlue = new Scalar(150, 255, 255);
+    Mat mask = new Mat();
+    Core.inRange(src, lowerBlue, upperBlue, mask);
     Imgproc.cvtColor(mask, mask, Imgproc.COLOR_GRAY2RGB);
     Mat result = new Mat();
     Core.subtract(mask, src, result);
@@ -85,9 +181,6 @@ public class Recognizer implements Runnable {
   }
 
   public void recognize() {
-//    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//    this.screenshotPath = screenshotPath;
-//    this.frame = frame;
     Imgproc.cvtColor(frame, frameGray, Imgproc.COLOR_RGB2GRAY);
 
     contours = findContouts(100);
@@ -95,13 +188,18 @@ public class Recognizer implements Runnable {
       return;
     }
 
-    String contourPath = screenshotPath + frameCount + "\\";
-    new File(contourPath).mkdirs();
+//    String contourPath = screenshotPath + frameCount + "\\";
+//    new File(contourPath).mkdirs();
     int i = 0;
     for (Contour c : contours) {
-      Mat plate = new Mat(frameGray, c.getRect());
+//      Mat plate = new Mat(frameGray, c.getRect());
+      Mat plate = new Mat(frame, c.getRect());
       Mat rotatedPlate = rotateImage(plate, c.getRotatedRect());
       Mat cuttedRotatedPlate = cutPlateFromRotatedPlate(rotatedPlate, c.getRotatedRect());
+
+      ////
+//      cuttedRotatedPlate = removeBlueColor(cuttedRotatedPlate);
+
 
       //recognize v 1.0
       String text = TextRecognizer.recognizeText(cuttedRotatedPlate);
@@ -111,7 +209,7 @@ public class Recognizer implements Runnable {
           Page9.results.add(text);
           Page9.rect = c.getRect();
           System.out.println(text);
-          Imgcodecs.imwrite(screenshotPath + "plate" + i + ".jpg", cuttedRotatedPlate);
+          Imgcodecs.imwrite(screenshotPath + "plate_"+frameCount+"_" + i + ".jpg", cuttedRotatedPlate);
 
         }
 
