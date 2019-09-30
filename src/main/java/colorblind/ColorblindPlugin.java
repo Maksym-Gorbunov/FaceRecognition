276d88bc9dbb8c3d30663a5611c9416be79e8aa2
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-09-30 16:20:44 +0200

    new grupp

--- src/main/java/colorblind/ColorblindPlugin.java
+++ src/main/java/colorblind/ColorblindPlugin.java
@@ -25,22 +25,61 @@ public class ColorblindPlugin {
   public static void main(String[] args) throws IOException {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     ColorblindPlugin colorblindPlugin = new ColorblindPlugin();
-    Mat image = Imgcodecs.imread(Constants.imgPath + "colorblind\\2.jpg");
+    Mat image = Imgcodecs.imread(Constants.imgPath + "colorblind\\1.png");
+    //colorblindPlugin.findColorConflict(image);    // my code
+
     //BufferedImage bufferedImage = ImageIO.read(new File(Constants.imgPath + "colorblind\\1.jpg"));
     //Mat img = bufferedImageToMat(bufferedImage);
     //Imgproc.cvtColor(img, img, Imgproc.COLOR_RGBA2RGB);
     //Imgcodecs.imwrite(path + "ddd.jpg", img);
-    Mat filtered = colorblindPlugin.colorblindFilter(image);
-    /*
+    //Mat filtered = colorblindPlugin.colorblindFilter2(image);
+
     long startTime = System.nanoTime();
     colorblindPlugin.findColorConflict(image);    // my code
     long endTime = System.nanoTime();
     long duration = (endTime - startTime);
     double seconds = (double)duration / 1_000_000_000.0;
     System.out.println("Execution time: " + seconds);
-    */
+
   }
 
+  // Simulate colorblind filter
+  private Mat colorblindFilter2(Mat img) {
+    Mat filtered = new Mat();
+    img.copyTo(filtered);
+
+
+    double blue = 0;
+    double green = 0;
+    double red = 0;
+
+    //List<Point>
+
+    for (int row = 0; row < img.rows(); row++) {
+      for (int col = 0; col < img.cols(); col++) {
+        double[] pixelBGR = img.get(row, col);
+        int pixelColorChannel = getColorChannel(pixelBGR);
+        //if pixel in red color family
+        if (pixelColorChannel == 3) {
+          Point point = new Point(col, row);
+          List<Point> neighbours = getNeighborPixels(img, point);
+
+          int mainColorChannel = getMainColor(neighbours, img);
+
+          if (mainColorChannel == 2) {
+            filtered.put(row, col, new double[]{0, 255, 0});
+          }
+          if (mainColorChannel == 3) {
+            filtered.put(row, col, new double[]{0, 0, 255});
+          }
+
+
+        }
+      }
+    }
+    Imgcodecs.imwrite(path + "filtered.jpg", filtered);
+    return filtered;
+  }
 
   // Simulate colorblind filter
   private Mat colorblindFilter(Mat img) {
@@ -79,14 +118,21 @@ public class ColorblindPlugin {
     int greenTotal = 0;
     for (Point p : points) {
       if (getColorChannel(img.get((int) p.y, (int) p.x)) == 2) {  // if pixel green
+        System.out.println(getColorChannel(img.get((int) p.y, (int) p.x)));
         greenTotal++;
       }
       if (getColorChannel(img.get((int) p.y, (int) p.x)) == 3) {  //if pixel red
+        System.out.println(getColorChannel(img.get((int) p.y, (int) p.x)));
         greenTotal++;
       }
     }
-    if ((greenTotal != 0) && (redTotal != 0) && (greenTotal > redTotal)) {
+    if (greenTotal > redTotal) {
+      System.out.println("green");
       return 2;
+    }
+    if (redTotal > greenTotal) {
+      System.out.println("red");
+      return 3;
     }
     return 1;
   }
@@ -138,71 +184,71 @@ public class ColorblindPlugin {
       //draw rects
       List<Rect> rectList = getRectangles(monoImg, img);
       System.out.println(rectList.size());
-//      if ((rectList != null) && (rectList.size() > 0)) {
-//        for (Rect rect : rectList) {
-//          Imgproc.rectangle(img, rect.tl(), rect.br(), new Scalar(255, 255, 255), 1);
-//        }
-//        Imgcodecs.imwrite(path + "rectangles.jpg", img);
-//      }
+      if ((rectList != null) && (rectList.size() > 0)) {
+        for (Rect rect : rectList) {
+          Imgproc.rectangle(img, rect.tl(), rect.br(), new Scalar(0, 0, 0), 2);
+        }
+        Imgcodecs.imwrite(path + "rectangles.jpg", img);
+      }
     } else {
       System.out.println("Color conflict not found");
     }
   }
 
 
-  private List<Rect> getRectangles(Mat monoImg, Mat img) {
-    Mat copy = new Mat();
-    img.copyTo(copy);
-    List<Rect> rectList = new ArrayList<>();
-    List<MatOfPoint> contours = new ArrayList<>();
-    Imgproc.findContours(monoImg, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-    for (MatOfPoint c : contours) {
-      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
-      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
-      if (rotatedRectangle.size.area() < 0.5 * monoImg.size().area()) {
-        Point rotatedRectPoints[] = new Point[4];
-        rotatedRectangle.points(rotatedRectPoints);
-        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
-        Imgproc.rectangle(monoImg, rect.tl(), rect.br(), white, -1);
-        Imgproc.rectangle(copy, rect.tl(), rect.br(), white, 1);
-        rectList.add(rect);
-      }
-    }
-    if (logger) {
-      Imgcodecs.imwrite(path + "mono_rects1.jpg", monoImg);
-      Imgcodecs.imwrite(path + "color_rects1.jpg", copy);
-    }
-    if (rectList.size() > 0) {
-      return groupRectangles(monoImg);
-    }
-    return null;
-  }
-
-  // Group mini rects groups in larger rects, combine intersecting bounding rectangles
-  private List<Rect> groupRectangles(Mat monoImg) {
-    List<Rect> rectList = new ArrayList<>();
-    List<MatOfPoint> contours = new ArrayList<>();
-    Imgproc.GaussianBlur(monoImg, monoImg, new Size(5, 5), 1, 1);
-    Imgproc.findContours(monoImg, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-    for (MatOfPoint c : contours) {
-      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
-      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
-      if (rotatedRectangle.size.area() < 0.5 * monoImg.size().area()) {
-        Point rotatedRectPoints[] = new Point[4];
-        rotatedRectangle.points(rotatedRectPoints);
-        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
-        Imgproc.rectangle(monoImg, rect.tl(), rect.br(), white, -1);
-        rectList.add(rect);
-      }
-    }
-    if (contours.size() > 0) {
-      if (logger) {
-        Imgcodecs.imwrite(path + "mono_rects2.jpg", monoImg);
-      }
-      return rectList;
-    }
-    return null;
-  }
+//  private List<Rect> getRectangles(Mat monoImg, Mat img) {
+//    Mat copy = new Mat();
+//    img.copyTo(copy);
+//    List<Rect> rectList = new ArrayList<>();
+//    List<MatOfPoint> contours = new ArrayList<>();
+//    Imgproc.findContours(monoImg, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+//    for (MatOfPoint c : contours) {
+//      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+//      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+//      if (rotatedRectangle.size.area() < 0.5 * monoImg.size().area()) {
+//        Point rotatedRectPoints[] = new Point[4];
+//        rotatedRectangle.points(rotatedRectPoints);
+//        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+//        Imgproc.rectangle(monoImg, rect.tl(), rect.br(), white, -1);
+//        Imgproc.rectangle(copy, rect.tl(), rect.br(), white, 1);
+//        rectList.add(rect);
+//      }
+//    }
+//    if (logger) {
+//      Imgcodecs.imwrite(path + "mono_rects1.jpg", monoImg);
+//      Imgcodecs.imwrite(path + "color_rects1.jpg", copy);
+//    }
+//    if (rectList.size() > 0) {
+//      return groupRectangles(monoImg);
+//    }
+//    return null;
+//  }
+//
+//  // Group mini rects groups in larger rects, combine intersecting bounding rectangles
+//  private List<Rect> groupRectangles(Mat monoImg) {
+//    List<Rect> rectList = new ArrayList<>();
+//    List<MatOfPoint> contours = new ArrayList<>();
+//    Imgproc.GaussianBlur(monoImg, monoImg, new Size(5, 5), 1, 1);
+//    Imgproc.findContours(monoImg, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+//    for (MatOfPoint c : contours) {
+//      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+//      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+//      if (rotatedRectangle.size.area() < 0.5 * monoImg.size().area()) {
+//        Point rotatedRectPoints[] = new Point[4];
+//        rotatedRectangle.points(rotatedRectPoints);
+//        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+//        Imgproc.rectangle(monoImg, rect.tl(), rect.br(), white, -1);
+//        rectList.add(rect);
+//      }
+//    }
+//    if (contours.size() > 0) {
+//      if (logger) {
+//        Imgcodecs.imwrite(path + "mono_rects2.jpg", monoImg);
+//      }
+//      return rectList;
+//    }
+//    return null;
+//  }
 
 
   // Cut off contours rectangle if out off image area, fix bug of OpenCV library
@@ -275,7 +321,7 @@ public class ColorblindPlugin {
     double blue = bgr[0];
     double green = bgr[1];
     double red = bgr[2];
-    int coefficient = 40;
+    int coefficient = 1;
     int channel = 0;
     if ((blue > green + coefficient) && (blue > red + coefficient)) {
       channel = 1;
@@ -297,5 +343,87 @@ public class ColorblindPlugin {
     byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
     mat.put(0, 0, data);
     return mat;
+  }
+
+
+  private List<Rect> getRectangles(Mat monoImg, Mat img) {
+    Mat copy = new Mat();
+    img.copyTo(copy);
+    List<Rect> rectList = new ArrayList<>();
+    List<MatOfPoint> contours = new ArrayList<>();
+    Imgproc.findContours(monoImg, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+    for (MatOfPoint c : contours) {
+      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+      if ((rotatedRectangle.size.area() < 0.5 * monoImg.size().area())) {
+        Point rotatedRectPoints[] = new Point[4];
+        rotatedRectangle.points(rotatedRectPoints);
+        Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+        if (rect.area() > 0) {
+          Imgproc.rectangle(monoImg, rect.tl(), rect.br(), white, -1);
+          Imgproc.rectangle(copy, rect.tl(), rect.br(), black, 2);
+          rectList.add(rect);
+        }
+      }
+    }
+    if (logger) {
+      Imgcodecs.imwrite(path + "mono_rects1.jpg", monoImg);
+      Imgcodecs.imwrite(path + "color_rects1.jpg", copy);
+    }
+    if (rectList.size() > 0) {
+      List<Rect> result = groupRectangles(monoImg);
+      return result;
+    }
+    return null;
+  }
+
+  // Group mini rects groups in larger rects, combine intersecting bounding rectangles
+  private List<Rect> groupRectangles(Mat monoImg) {
+    List<Rect> rectList = new ArrayList<>();
+    List<MatOfPoint> contours = new ArrayList<>();
+    Imgproc.GaussianBlur(monoImg, monoImg, new Size(5, 5), 1, 1);
+    Imgproc.findContours(monoImg, contours, new Mat(), RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+    for (MatOfPoint c : contours) {
+      MatOfPoint2f pointsArea = new MatOfPoint2f(c.toArray());
+      RotatedRect rotatedRectangle = Imgproc.minAreaRect(pointsArea);
+//      if ( (rotatedRectangle.size.area() > 500)
+//             && (rotatedRectangle.size.area() < 0.5 * monoImg.size().area())) {
+      Point rotatedRectPoints[] = new Point[4];
+      rotatedRectangle.points(rotatedRectPoints);
+      Rect rect = Imgproc.boundingRect(new MatOfPoint(rotatedRectPoints));
+      Imgproc.rectangle(monoImg, rect.tl(), rect.br(), white, -1);
+      rectList.add(rect);
+    }
+
+    rectList = removeInnerRects(rectList);
+
+    //}
+    if (contours.size() > 0) {
+      if (logger) {
+        Imgcodecs.imwrite(path + "mono_rects2.jpg", monoImg);
+      }
+      return rectList;
+    }
+    return null;
+  }
+
+  private List<Rect> removeInnerRects(List<Rect> rectList) {
+    List<Rect> cleanedRects = new ArrayList<>();
+    for (Rect rect : rectList) {
+      boolean inside = false;
+      for (Rect r : rectList) {
+//        Point tl = r.tl();
+//        Point br = rect.br();
+//        Point tr = new Point(tl.x+rect.width,tl.y);
+//        Point bl = new Point(br.x-rect.width, br.y);
+        if ((r.contains(rect.tl())) && (r.contains(rect.br()))) {
+          inside = true;
+        }
+      }
+      if (!inside && rect.area()>500) {
+        cleanedRects.add(rect);
+      }
+    }
+    return cleanedRects;
   }
 }
